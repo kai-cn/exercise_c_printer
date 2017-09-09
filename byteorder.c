9c8f459b336e1263bb9927be5cd466ba14ce748f
kklin <kai.lin@szjlxh.com>
2017-09-09 22:47:51 +0800

    upload byte order

---
+++ byteorder.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int hoge = 0x12345678;
+
+    unsigned char* hoge_p = (unsigned char*)&hoge;
+
+    printf("hoge size..%d\n",sizeof(hoge));
+    printf("%x\n",hoge_p[0]);
+    printf("%x\n",hoge_p[1]);
+    printf("%x\n",hoge_p[2]);
+    printf("%x\n",hoge_p[3]);
+
+    return 0;
+}
